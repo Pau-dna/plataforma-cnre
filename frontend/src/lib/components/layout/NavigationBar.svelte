8a8e6e4360930572e42e1dbe961d66042c0a4f53
Pau-dna <pasuarezgu@unal.edu.co>
2025-08-16 01:31:01 -0500

    feat: agregar barra de navegación

---
+++ frontend/src/lib/components/layout/NavigationBar.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+	import { Bell, type Icon as IconType } from '@lucide/svelte';
+
+	type NavLink = {
+		href: string;
+		name: string;
+	};
+
+	type Props = {
+		links: NavLink[];
+	};
+
+	const { links }: Props = $props();
+
+	let activeRoute = $derived(page?.url?.pathname);
+</script>
+
+<nav class="flex items-center justify-between border-b md:px-12 xl:px-24">
+	<div class="flex items-center gap-x-20">
+		<div class="flex shrink-0 text-lg font-semibold">
+			<a href="/home" class="flex shrink-0 py-4">
+				<span class="text-2xl font-bold">CNRE</span>
+			</a>
+		</div>
+
+		<div class="flex items-center gap-x-6">
+			{#each links as link}
+				<a href={link?.href || ''}>
+					<div
+						class={`relative flex items-center justify-center gap-2 border-b-2 px-2 py-6 text-base font-medium transition-colors duration-200  ${
+							activeRoute === link.href
+								? 'border-b-2 border-indigo-500'
+								: 'border-transparent text-primary/80 hover:border-gray-200 hover:text-primary'
+						}`}
+					>
+						<span>{link.name}</span>
+					</div>
+				</a>
+			{/each}
+		</div>
+	</div>
+
+	<div class="py-4">
+		<Avatar.Root class="size-10">
+			<Avatar.Fallback class="bg-gradient-to-r from-indigo-400 to-pink-400"></Avatar.Fallback>
+		</Avatar.Root>
+	</div>
+</nav>
---
+++ frontend/src/routes/(authed)/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	import { CirclePlay, House } from '@lucide/svelte';
+	import NavigationBar from '$lib/components/layout/NavigationBar.svelte';
+	import type { Icon as IconType } from '@lucide/svelte';
+
+	let { children }: { data: LayoutData; children: Snippet } = $props();
+
+	type NavLink = {
+		href: string;
+		name: string;
+	};
+
+	const links: NavLink[] = [
+		{
+			name: 'Inicio',
+			href: '/inicio',
+		},
+		{
+			href: '/mis-cursos',
+			name: 'Mis Cursos',
+		}
+	];
+</script>
+
+<div class="hidden md:block">
+	<NavigationBar {links} />
+</div>
+
+<div class="px-4 pt-12 pb-24 md:px-12 md:pb-12 xl:px-24">
+	{@render children()}
+</div>
