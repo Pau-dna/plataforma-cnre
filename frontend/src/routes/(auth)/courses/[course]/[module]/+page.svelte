e089b5a552dd9a4b84633062c50b7b9f57a9c7db
imlargo <jlargob@unal.edu.co>
2025-09-07 22:32:49 -0500

    refactor: enhance layout structure for module details display

--- frontend/src/routes/(auth)/courses/[course]/[module]/+page.svelte
+++ frontend/src/routes/(auth)/courses/[course]/[module]/+page.svelte
@@ -6,7 +6,13 @@
 	const modulo = $derived(data.modules.find(m => m.id === data.moduleID))
 </script>
 
-<div class="flex flex-col gap-y-4">
-	<h1 class="text-3xl font-bold">{modulo.title}</h1>
+<div class="flex flex-col gap-y-8">
+	<div  class="flex flex-col gap-y-2">
+		<h1 class="text-3xl font-bold">{modulo.title}</h1>
 	<p class="text-muted-foreground">{modulo.description}</p>
+	</div>
+
+	<div>
+		<h2 class="font-medium text-muted-foreground">Contenidos</h2>
+	</div>
 </div>
