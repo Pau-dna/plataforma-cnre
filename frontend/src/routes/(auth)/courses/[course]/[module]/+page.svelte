4187cfd66474b6fe5909f735f466fd4aa6900e9b
imlargo <jlargob@unal.edu.co>
2025-09-07 22:31:31 -0500

    feat: implement page load function to retrieve module ID and display module details

--- frontend/src/routes/(auth)/courses/[course]/[module]/+page.svelte
+++ frontend/src/routes/(auth)/courses/[course]/[module]/+page.svelte
@@ -2,4 +2,11 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	const modulo = $derived(data.modules.find(m => m.id === data.moduleID))
 </script>
+
+<div class="flex flex-col gap-y-4">
+	<h1 class="text-3xl font-bold">{modulo.title}</h1>
+	<p class="text-muted-foreground">{modulo.description}</p>
+</div>
