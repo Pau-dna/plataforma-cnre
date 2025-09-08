477c12b757cb5a2f44f1eb76714b8a02c4af7272
imlargo <jlargob@unal.edu.co>
2025-09-07 21:04:22 -0500

    add CourseSidebar component and integrate into layout

---
+++ frontend/src/routes/(auth)/courses/[course]/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import CourseSidebar from '$lib/components/course/CourseSidebar.svelte';
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+<div class="relative">
+	<Sidebar.Provider>
+		<CourseSidebar />
+		<main>
+			<Sidebar.Trigger />
+			<div>
+				{@render children?.()}
+			</div>
+		</main>
+	</Sidebar.Provider>
+</div>
