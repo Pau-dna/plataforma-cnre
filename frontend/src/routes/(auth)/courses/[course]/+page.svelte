df4da27f9b297ede9a5624c2feedbe2d41606346
imlargo <jlargob@unal.edu.co>
2025-09-08 09:33:43 -0500

    _

--- frontend/src/routes/(auth)/courses/[course]/+page.svelte
+++ frontend/src/routes/(auth)/courses/[course]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { CheckCircle } from '@lucide/svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
@@ -10,11 +11,27 @@
 
 	<div class="grid grid-cols-12 gap-8">
 		<div class="col-span-8">
-			<div class="bg-muted aspect-[16/6] w-full rounded-lg border"></div>
+			<div class="bg-indigo-50 aspect-[16/6] w-full rounded-lg ">
+			</div>
 		</div>
 
-		<div class="col-span-4">
+		<div class="col-span-4 flex flex-col gap-y-2">
 			<h2 class="text-h2">Tu progreso</h2>
+
+			{#each data.modules as modulo}
+			<div class="flex gap-x-2 items-center rounded-lg border bg-muted/5 p-3">
+				<CheckCircle class="size-4 text-lime-500" />
+				<p class="text-muted-foreground text-sm font-medium">{modulo.title}</p>
+			</div>	
+			{/each}
+			
 		</div>
 	</div>
 </div>
+
+
+<style>
+	.bg {
+
+	}
+</style>
