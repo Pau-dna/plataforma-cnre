e72e3267675287b4d527a645a2df4ea0e32ef214
Pau-dna <pasuarezgu@unal.edu.co>
2025-09-08 00:59:25 -0500

    feat: enhance course page layout and styles; add subtitle styling in app.css

--- frontend/src/routes/(auth)/admin/courses/[course]/+page.svelte
+++ frontend/src/routes/(auth)/admin/courses/[course]/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-    import type { PageProps } from './$types';
+	import type { PageProps } from './$types';
 
-    let { data }: PageProps = $props();
+	let { data }: PageProps = $props();
 </script>
+
+<div class="flex flex-col gap-4">
+	<div class="flex flex-col gap-2">
+		<h1 class="text-h1">Módulos del Curso</h1>
+		<p class="text-subtitle">Inducción al CNRE</p>
+	</div>
+</div>
