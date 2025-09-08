33077be26455ad177753f614a25071db4aedc16f
imlargo <jlargob@unal.edu.co>
2025-09-07 22:22:45 -0500

    refactor layout component to improve breadcrumb navigation and structure

--- frontend/src/routes/(auth)/courses/[course]/+layout.svelte
+++ frontend/src/routes/(auth)/courses/[course]/+layout.svelte
@@ -2,7 +2,9 @@
 	import type { LayoutProps } from './$types';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import CourseSidebar from '$lib/components/course/CourseSidebar.svelte';
-	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { page } from '$app/state';
 
 	let { data, children }: LayoutProps = $props();
 </script>
@@ -14,6 +16,24 @@
 			<div class="flex items-center gap-x-2">
 				<Sidebar.Trigger />
 				<Separator orientation="vertical" />
+
+				<Breadcrumb.Root>
+					<Breadcrumb.List>
+						<Breadcrumb.Item>
+							<Breadcrumb.Link href="/courses/{data.course.id}">{data.course.title}</Breadcrumb.Link
+							>
+						</Breadcrumb.Item>
+						{#if page.params.module}
+							<Breadcrumb.Separator />
+							<Breadcrumb.Item>
+								<Breadcrumb.Link href="/docs/components"
+									>{data.modules.find((m) => m.id === parseInt(page.params.module || "-1"))
+										?.title}</Breadcrumb.Link
+								>
+							</Breadcrumb.Item>
+						{/if}
+					</Breadcrumb.List>
+				</Breadcrumb.Root>
 			</div>
 
 			<div>
