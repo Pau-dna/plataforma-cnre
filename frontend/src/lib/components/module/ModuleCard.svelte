86b4017a053649d297f3e963429ebd4b5b9f6de1
Pau-dna <pasuarezgu@unal.edu.co>
2025-09-08 09:59:24 -0500

    feat: implement ModuleCard component for course modules; enhance course page with module listing

---
+++ frontend/src/lib/components/module/ModuleCard.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { ArrowDown, ArrowUp, BookOpen, CirclePlay, Ellipsis, GripVertical } from '@lucide/svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+
+	type Props = {
+		id: number;
+		title: string;
+		description: string;
+	};
+
+	const { id, title, description }: Props = $props();
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<div class="flex items-center justify-between">
+			<div class="flex items-center gap-2">
+				<Button size="sm" variant="ghost" class="bg-muted">
+					<GripVertical class="text-muted-foreground h-4 w-4" />
+				</Button>
+
+				<span class="text-muted-foreground font-semibold">Modulo {id}</span>
+			</div>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					<Button size="sm" variant="ghost">
+						<Ellipsis class="h-4 w-4 leading-none" />
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						<DropdownMenu.Item>Ver Detalles</DropdownMenu.Item>
+						<DropdownMenu.Item>Editar</DropdownMenu.Item>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item class="text-destructive">Eliminar</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</div>
+		<div class="flex items-center gap-4">
+			<div class="flex flex-col gap-0">
+				<Button size="sm" variant="ghost">
+					<ArrowUp class="h-4 w-4" />
+				</Button>
+				<Button size="sm" variant="ghost">
+					<ArrowDown class="h-4 w-4" />
+				</Button>
+			</div>
+			<div class="flex flex-col gap-1">
+				<Card.Title class="text-lg">{title}</Card.Title>
+				<Card.Description>{description}</Card.Description>
+			</div>
+		</div>
+	</Card.Header>
+	<Card.Content class="flex gap-4">
+		<div class="flex items-center gap-2">
+			<CirclePlay class="text-muted-foreground h-4 w-4 leading-none" />
+			<span class="text-muted-foreground text-sm leading-none">Video Incluido</span>
+		</div>
+		<div class="flex items-center gap-2 text-cyan-900">
+			<BookOpen class="h-4 w-4 leading-none" />
+			<span class="text-sm leading-none">Ver Contenido</span>
+		</div>
+	</Card.Content>
+</Card.Root>
--- frontend/src/routes/(auth)/admin/courses/[course]/+page.svelte
+++ frontend/src/routes/(auth)/admin/courses/[course]/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import ModuleCard from '$lib/components/module/ModuleCard.svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	const modules = [
+		{ id: 1, title: 'Introducción al CNRE', description: 'Descripción del módulo 1', },
+		{ id: 2, title: 'Estructura del CNRE', description: 'Descripción del módulo 2' },
+		{ id: 3, title: 'Funciones y Responsabilidades', description: 'Descripción del módulo 3' }
+	];
 </script>
 
 <div class="flex flex-col gap-4">
@@ -9,4 +16,10 @@
 		<h1 class="text-h1">Módulos del Curso</h1>
 		<p class="text-subtitle">Inducción al CNRE</p>
 	</div>
+
+    <div class="grid grid-cols-1 gap-4">
+        {#each modules as module}
+            <ModuleCard id={module.id} title={module.title} description={module.description} />
+        {/each}
+    </div>
 </div>
