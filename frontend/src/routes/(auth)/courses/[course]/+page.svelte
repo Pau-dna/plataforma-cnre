4eece87766a765a4df1182d536e7d4f77ac18926
imlargo <jlargob@unal.edu.co>
2025-09-07 22:27:39 -0500

    refactor: adjust layout structure and improve styling for course page

--- frontend/src/routes/(auth)/courses/[course]/+page.svelte
+++ frontend/src/routes/(auth)/courses/[course]/+page.svelte
@@ -6,9 +6,15 @@
 
 <div class="flex flex-col gap-y-4">
 	<h1 class="text-h1">{data.course.title}</h1>
-	<p class="text-pretty max-w-prose text-muted-foreground">{data.course.description}</p>
+	<p class="text-muted-foreground max-w-prose text-pretty">{data.course.description}</p>
 
-	<div class="aspect-[16/6] w-full bg-muted border rounded-lg">
+	<div class="grid grid-cols-12 gap-8">
+		<div class="col-span-8">
+			<div class="bg-muted aspect-[16/6] w-full rounded-lg border"></div>
+		</div>
 
+		<div class="col-span-4">
+			<h2 class="text-h2">Tu progreso</h2>
+		</div>
 	</div>
 </div>
