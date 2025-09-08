74a4b670d906dba19048f12f2f33d981c60a2e48
imlargo <jlargob@unal.edu.co>
2025-09-07 23:30:13 -0500

    _

--- backend/internal/models/course.go
+++ backend/internal/models/course.go
@@ -3,17 +3,16 @@ package models
 // Course - modelo de curso
 type Course struct {
 	BaseModel
-	Title            string  `json:"title" gorm:"not null"`
-	Description      string  `json:"description" gorm:"type:text"`
-	ShortDescription *string `json:"shortDescription" gorm:"column:short_description;type:text"`
-	ImageURL         *string `json:"imageUrl" gorm:"column:image_url"`
-	StudentCount     *int    `json:"student_count" gorm:"column:student_count"`
-	ModuleCount      *int    `json:"module_count" gorm:"column:module_count"`
+	Title            string `json:"title" gorm:"not null"`
+	Description      string `json:"description"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	StudentCount     int    `json:"student_count"`
+	ModuleCount      int    `json:"module_count"`
 
 	// Relaciones
-	Modules      []Module       `json:"modules" gorm:"foreignKey:CourseID"`
-	Enrollments  []Enrollment   `json:"enrollments" gorm:"foreignKey:CourseID"`
-	UserProgress []UserProgress `json:"user_progress" gorm:"foreignKey:CourseID"`
+	Modules     []*Module     `json:"modules" gorm:"foreignKey:CourseID"`
+	Enrollments []*Enrollment `json:"enrollments" gorm:"foreignKey:CourseID"`
 }
 
 func (Course) TableName() string {
