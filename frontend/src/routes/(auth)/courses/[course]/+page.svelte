3397500b217d1bb17b02ca3897f91c317906505a
imlargo <jlargob@unal.edu.co>
2025-09-07 22:25:47 -0500

    refactor: restructure course page layout to display title and description

--- frontend/src/routes/(auth)/courses/[course]/+page.svelte
+++ frontend/src/routes/(auth)/courses/[course]/+page.svelte
@@ -4,4 +4,11 @@
 	let { data }: PageProps = $props();
 </script>
 
-teamo
+<div class="flex flex-col gap-y-4">
+	<h1 class="text-h1">{data.course.title}</h1>
+	<p class="text-pretty max-w-prose text-muted-foreground">{data.course.description}</p>
+
+	<div class="aspect-[16/6] w-full bg-muted border rounded-lg">
+
+	</div>
+</div>
