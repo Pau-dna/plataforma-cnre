ec64c9aa7f0f7b24c80c4b6f79120528b4f01028
imlargo <jlargob@unal.edu.co>
2025-09-22 02:48:52 -0500

    _

--- frontend/src/lib/components/login/LoginCard.svelte
+++ frontend/src/lib/components/login/LoginCard.svelte
@@ -2,6 +2,41 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { ArrowRight } from '@lucide/svelte';
+	import { googleClientID } from '$lib/constants';
+
+	async function handleLogin() {
+		// Google's OAuth 2.0 endpoint for requesting an access token
+		const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+		// Create <form> element to submit parameters to OAuth 2.0 endpoint.
+		const form = document.createElement('form');
+		form.setAttribute('method', 'GET'); // Send as a GET request.
+		form.setAttribute('action', oauth2Endpoint);
+
+		// Parameters to pass to OAuth 2.0 endpoint.
+		const params: Record<string, string> = {
+			client_id: googleClientID,
+			redirect_uri: `${window.location.origin}/authorize`,
+			response_type: 'code',
+			prompt: 'select_account',
+			scope: 'openid profile email',
+			include_granted_scopes: 'true'
+		};
+
+		// Add form parameters as hidden input values.
+		for (const p in params) {
+			const input = document.createElement('input');
+			input.setAttribute('type', 'hidden');
+			input.setAttribute('name', p);
+			input.setAttribute('value', params[p]);
+			form.appendChild(input);
+		}
+
+		// Add form to page and submit it to open the OAuth 2.0 endpoint.
+		document.body.appendChild(form);
+		form.submit();
+	}
+
 </script>
 
 <Card.Root class="w-md border-sky-500/25 shadow-lg shadow-sky-500/25">
