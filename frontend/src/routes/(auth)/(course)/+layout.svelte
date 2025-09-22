37b4de5f97b36ba396a080a94d18e2027ad885b1
imlargo <jlargob@unal.edu.co>
2025-09-21 23:25:22 -0500

    fix layout

---
+++ frontend/src/routes/(auth)/(course)/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { LayoutProps } from './$types';
+
+    let { data, children }: LayoutProps = $props();
+</script>
+
+<div class="pb-24 md:pb-12">
+	{@render children()}
+</div>
