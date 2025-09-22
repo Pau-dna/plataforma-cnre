68934ef1f0b9461101ab3919cfe0246321e0ee19
imlargo <jlargob@unal.edu.co>
2025-09-22 12:05:08 -0500

    _

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import '$styles/app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children?.()}
 
