9212f238bb0bf61de00c8d92291a3c53d72e9955
imlargo <jlargob@unal.edu.co>
2025-09-22 22:52:31 -0500

    _

--- frontend/src/lib/constants/index.ts
+++ frontend/src/lib/constants/index.ts
@@ -1,3 +1,4 @@
-import { PUBLIC_GOOGLE_CLIENT } from '$env/static/public';
+import { PUBLIC_GOOGLE_CLIENT, API_URL } from '$env/static/public';
 
 export const googleClientID = PUBLIC_GOOGLE_CLIENT || '';
+export const apiUrl = API_URL || '';
