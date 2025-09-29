51a31b79a67092f9fe851e9089d79b5f71f12675
imlargo <jlargob@unal.edu.co>
2025-09-28 20:30:58 -0500

    Implement enrollment retrieval and redirect logic in server load function

--- frontend/src/routes/(auth)/certificado/[enrollment]/+page.server.ts
+++ frontend/src/routes/(auth)/certificado/[enrollment]/+page.server.ts
@@ -1,6 +1,26 @@
 
+import { EnrollmentController, type Enrollment } from '$lib';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    return {};
+export const load = (async ({ params }) => {
+
+    const enrollmentController = new EnrollmentController();
+
+    let enrollment: Enrollment | null = null;
+    let redirectPath = "";
+    try {
+        enrollment = await enrollmentController.getEnrollment(parseInt(params.enrollment));
+        if (!enrollment || enrollment.progress < 100) {
+            redirectPath = "/my-courses";
+        }
+    } catch (error) {
+        redirectPath = "/my-courses";
+    }
+
+    if (redirectPath) {
+        redirect(307, redirectPath)
+    }
+
+    return { enrollment: enrollment as Enrollment };
 }) satisfies PageServerLoad;
